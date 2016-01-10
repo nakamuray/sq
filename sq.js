1e4ef30cf7694667d60df718bd13ecbba5028296
NAKAMURA Yoshitaka <arumakanoy@gmail.com>
2016-01-10 18:45:52 +0900

    process multiple URLs

--- sq.js
+++ sq.js
@@ -8,55 +8,61 @@
         var coffeeScript = require('coffee-script');
         var jsdom = require('jsdom');
 
-        jsdom.env(
-            urlOrFilenameOrHtml,
-            [],
-            {features: {
-                FetchExternalResources: false,
-                ProcessExternalResources: false
-            }},
-            function(error, window) {
-                if (error) {
-                    console.error(error);
-                    process.exit(1);
-                }
-
-                var jquery = require('jquery')(window);
-
-                global.$ = jquery;
-                global.document = window.document;
-                global.window = window;
-
-                //var result = eval(script);
-                var result = coffeeScript.eval(script);
-
-                if (!(util.isArray(result) || result instanceof jquery)) {
-                    result = [result];
-                }
-
-                Array.prototype.forEach.call(result, function(elem) {
-                    if (elem instanceof window.HTMLElement) {
-                        elem = elem.outerHTML;
+        var promise = new Promise(function(resolve, reject) {
+            jsdom.env(
+                urlOrFilenameOrHtml,
+                [],
+                {features: {
+                    FetchExternalResources: false,
+                    ProcessExternalResources: false
+                }},
+                function(error, window) {
+                    if (error) {
+                        console.error(error);
+                        reject(error);
                     }
-                    console.log(elem);
-                });
-            }
-        );
+
+                    var jquery = require('jquery')(window);
+
+                    global.$ = jquery;
+                    global.document = window.document;
+                    global.window = window;
+
+                    //var result = eval(script);
+                    var result = coffeeScript.eval(script);
+
+                    if (!(util.isArray(result) || result instanceof jquery)) {
+                        result = [result];
+                    }
+
+                    Array.prototype.forEach.call(result, function(elem) {
+                        if (elem instanceof window.HTMLElement) {
+                            elem = elem.outerHTML;
+                        }
+                        console.log(elem);
+                    });
+                    resolve();
+                }
+            );
+        });
+
+        return promise;
     }
 
     if (process.argv.length < 3) {
-        console.error('Usage: ' + process.argv[1] + ' <coffee expression> [URL or filename]');
+        console.error('Usage: ' + process.argv[1] + ' <coffee expression> [URL or filename...]');
         process.exit(1);
     }
 
     var script = process.argv[2];
+    var args = process.argv.slice(3);
 
-    if (process.argv.length >= 4) {
-        var urlOrFilename = process.argv[3];
-        sq(script, urlOrFilename);
-
-        process.stdout.once('drain', function() {
-            process.exit();
+    if (args.length) {
+        var promise = Promise.resolve();
+        args.forEach((urlOrFilename) => {
+            promise = promise.then(() => {
+                return sq(script, urlOrFilename);
+            });
         });
     } else {
         var html = '';
@@ -68,10 +74,6 @@
         });
         process.stdin.on('end', function() {
             sq(script, html);
-
-            process.stdout.once('drain', function() {
-                process.exit();
-            });
         });
     }
 }());
