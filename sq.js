b6e70411709e0884b00488ba0f1b68257a54b767
NAKAMURA Yoshitaka <arumakanoy@gmail.com>
2016-01-11 18:01:28 +0900

    use minimist to parse command line options
    ... and add "help" and "quiet" options.

--- sq.js
+++ sq.js
@@ -2,7 +2,14 @@
 (function () {
     'use strict';
 
-    function sq(script, urlOrFilenameOrHtml) {
+    const usage = `Usage: sq [option]... <coffee expression> [URL or file]...
+        Options:
+            -h, --help    display this message
+            -n, --quiet   surpress automatic result printing`
+
+    function sq(script, urlOrFilenameOrHtml, options) {
+        options = options || {};
+
         var util = require('util');
 
         var coffeeScript = require('coffee-script');
@@ -35,12 +42,14 @@
                         result = [result];
                     }
 
-                    Array.prototype.forEach.call(result, function(elem) {
-                        if (elem instanceof window.HTMLElement) {
-                            elem = elem.outerHTML;
-                        }
-                        console.log(elem);
-                    });
+                    if (!options.quiet) {
+                        Array.prototype.forEach.call(result, function(elem) {
+                            if (elem instanceof window.HTMLElement) {
+                                elem = elem.outerHTML;
+                            }
+                            console.log(elem);
+                        });
+                    }
                     resolve();
                 }
             );
@@ -49,19 +58,28 @@
         return promise;
     }
 
-    if (process.argv.length < 3) {
-        console.error('Usage: ' + process.argv[1] + ' <coffee expression> [URL or filename...]');
+    var parserArgs = require('minimist');
+    var opts = parserArgs(process.argv.slice(2), {
+        boolean: ['help', 'quiet'],
+        alias: {h: 'help', n: 'quiet'}
+    });
+
+    if (opts.help) {
+        console.log(usage);
+        process.exit(0);
+    } else if (!opts._.length) {
+        console.error(usage);
         process.exit(1);
     }
 
-    var script = process.argv[2];
-    var args = process.argv.slice(3);
+    var script = opts._[0];
+    var args = opts._.slice(1);
 
     if (args.length) {
         var promise = Promise.resolve();
         args.forEach((urlOrFilename) => {
             promise = promise.then(() => {
-                return sq(script, urlOrFilename);
+                return sq(script, urlOrFilename, opts);
             });
         });
     } else {
@@ -73,7 +91,7 @@
             }
         });
         process.stdin.on('end', function() {
-            sq(script, html);
+            sq(script, html, opts);
         });
     }
 }());
