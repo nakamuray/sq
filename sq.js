0035aab7d540a0523572d64f68056f392bfab274
NAKAMURA Yoshitaka <arumakanoy@gmail.com>
2016-01-11 18:55:26 +0900

    add command line options for jsdom

--- sq.js
+++ sq.js
@@ -4,11 +4,30 @@
 
     const usage = `Usage: sq [option]... <coffee expression> [URL or file]...
         Options:
-            -h, --help    display this message
-            -n, --quiet   surpress automatic result printing`
+            -h, --help              display this message
+            -n, --quiet             surpress automatic result printing
+            -b, --cookie=key=value  mannualy set cookie value
+            -r, --referrer=URL      set referrer to URL
+            -A, --user-agent=AGENT  set User-Agent to AGENT`
 
     function sq(script, urlOrFilenameOrHtml, options) {
-        options = options || {};
+        var config = {
+            features: {
+                FetchExternalResources: false,
+                ProcessExternalResources: false
+            }
+        };
+        Object.assign(config, {
+            referrer: options.referrer,
+            cookie: options.cookie,
+            userAgent: options.userAgent
+        });
+
+        if (config.referrer) {
+            config.headers = {
+                Referer: config.referrer
+            };
+        }
 
         var coffeeScript = require('coffee-script');
         var jsdom = require('jsdom');
@@ -17,10 +36,7 @@
             jsdom.env(
                 urlOrFilenameOrHtml,
                 [],
-                {features: {
-                    FetchExternalResources: false,
-                    ProcessExternalResources: false
-                }},
+                config,
                 function(error, window) {
                     if (error) {
                         console.error(error);
@@ -58,8 +74,16 @@
 
     var parserArgs = require('minimist');
     var opts = parserArgs(process.argv.slice(2), {
+        string: ['cookie', 'referrer', 'user-agent'],
         boolean: ['help', 'quiet'],
-        alias: {h: 'help', n: 'quiet'}
+        alias: {
+            b: 'cookie',
+            r: 'referrer',
+            h: 'help',
+            n: 'quiet',
+            A: 'userAgent',
+            'user-agent': 'userAgent'
+        }
     });
 
     if (opts.help) {
